825325aa3a59b418d73e210c4a9129f416bc8c04
Alberto Fanton <alberto.fanton@argo.vision>
2019-02-11 16:29:31 +0100

    Added tests for decorators

--- test/decorators.py
+++ test/decorators.py
@@ -15,6 +15,70 @@ class TestDecoratorAddedInfo(unittest.TestCase):
         self.assertIsNotNone(iface.get('method'))
         self.assertEqual('DELETE', iface.get('method'))
 
+    def test_post(self):
+        @ps.post()
+        def a_post_op(): pass
+
+        iface = a_post_op.rest_interface
+        self.assertIsNotNone(iface)
+
+        method = iface.get('method')
+        self.assertIsNotNone(method)
+        self.assertEqual('POST', method)
+
+        codec = iface.get('consumes')
+        self.assertIsNotNone(codec)
+        self.assertTrue(issubclass(codec, ps.Codec))
+        self.assertEqual(ps.JSON, codec)
+
+    def test_put(self):
+        @ps.put()
+        def a_put_op(): pass
+
+        iface = a_put_op.rest_interface
+        self.assertIsNotNone(iface)
+
+        method = iface.get('method')
+        self.assertIsNotNone(method)
+        self.assertEqual('PUT', method)
+
+        codec = iface.get('consumes')
+        self.assertIsNotNone(codec)
+        self.assertTrue(issubclass(codec, ps.Codec))
+        self.assertEqual(ps.JSON, codec)
+
+    def test_get(self):
+        @ps.get()
+        def a_get_op(): pass
+
+        iface = a_get_op.rest_interface
+        self.assertIsNotNone(iface)
+
+        method = iface.get('method')
+        self.assertIsNotNone(method)
+        self.assertEqual('GET', method)
+
+        codec = iface.get('produces')
+        self.assertIsNotNone(codec)
+        self.assertTrue(issubclass(codec, ps.Codec))
+        self.assertEqual(ps.JSON, codec)
+
+    def test_get_list(self):
+        @ps.get()
+        def a_get_list_op(): pass
+
+        iface = a_get_list_op.rest_interface
+        self.assertIsNotNone(iface)
+
+        method = iface.get('method')
+        self.assertIsNotNone(method)
+        self.assertEqual('GET', method)
+
+        codec = iface.get('produces')
+        self.assertIsNotNone(codec)
+        self.assertTrue(issubclass(codec, ps.Codec))
+        self.assertEqual(ps.JSON, codec)
+
 
 if __name__ == '__main__':
     unittest.main()
