3faca136f699357d7a62e3db0e8cfc1e5bcb710b
Alberto Fanton <alberto.fanton@argo.vision>
2019-03-05 18:59:15 +0100

    Updated package exports

--- pyservices/__init__.py
+++ pyservices/__init__.py
@@ -1,6 +1,7 @@
 # This is the module facade, importing classes from the src dir/module.
-from .decorators import rest, get, post, put, delete, get_list
+from .decorators import rest_add, rest_collection, rest_delete, \
+    rest_detail, rest_update
 from .entity_codecs import JSON, Codec
 from .layer_supertypes import Model, Service
 from .tools import rest_server, rest_client
-from . import content_types
+from . import content_types, frameworks
